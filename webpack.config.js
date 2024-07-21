e9bc4afb1c683113654339ae0500679d2f26def4
fredydlemus <fflores@eaglehubs.com>
2024-07-21 07:18:25 -0600

    adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    target: "node",
+    mode: "production",
+}
